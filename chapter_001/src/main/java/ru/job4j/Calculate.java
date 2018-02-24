48853d0f20e0560122a737a8f4858b7fbbc0965d
Eddie Eihenb <eihenb3333@gmail.com>
2018-02-24 08:15:38 +0000

    added jdoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,13 @@
 package ru.job4j;
-
+/**
+* Calculate.
+* @Author Eddie Eihenb.
+*/
 public class Calculate {
+	/**
+	*@return - string.
+	*@param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("We will not use hello world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for output Hello world!
+*
+*@author Eddie Eihenb
+*@version $Id$
+*@since 0.1
+*/
+package ru.job4j
